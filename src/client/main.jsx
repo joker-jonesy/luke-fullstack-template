3e04ed3e5e6891d66b7dc3c2a086e60ffa99ba88
joker-jonesy <spashmcgrath94@yahoo.com>
2023-10-10 09:20:32 -0400

    updated front end

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import store from "./store";
+import Nav from "./components/Nav";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <Nav/>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
