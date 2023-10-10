fddd0b46579cde3085e9149510d7a90668ab98e8
joker-jonesy <spashmcgrath94@yahoo.com>
2023-10-10 12:32:25 -0400

    Update main.js

--- src/server/main.js
+++ src/server/main.js
@@ -7,6 +7,12 @@ app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
+app.get("/sauce", (req, res) => {
+  res.send("This class is the sauce");
+});
+
+// backend routes
+
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
